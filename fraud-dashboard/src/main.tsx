75953a32fb736eb1d0dcd1063ab4df74c2c671fa
clone891 <mrclone136@gmail.com>
2026-07-24 02:50:59 +0530

    feat: wrap App in ThemeProvider in main.tsx

--- fraud-dashboard/src/main.tsx
+++ fraud-dashboard/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 import App from "./App";
 import "./index.css";
@@ -12,9 +13,11 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <App />
-      </TooltipProvider>
+      <ThemeProvider>
+        <TooltipProvider>
+          <App />
+        </TooltipProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
